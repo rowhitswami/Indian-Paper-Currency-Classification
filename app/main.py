8b7a6d69a231fe7d83023a238905f12e38170d7e
Rohit Swami <rowhitswami1@gmail.com>
2020-05-22 03:27:27 +0530

    Add S3 support

--- app/main.py
+++ app/main.py
@@ -2,19 +2,21 @@ from flask_cors import CORS
 import numpy as np
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array, load_img
-import os
 import urllib.request
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 import tensorflow as tf
 from keras.backend import set_session
+import boto3
+import io
+from config import LABELS, ALLOWED_EXTENSIONS, AWS_ACCESS_KEY, AWS_SECRET_KEY
+from config import BUCKET, UPLOAD_FOLDER, REGION_HOST, FLASK_SECRET_KEY
 
 app = Flask(__name__)
+app.secret_key = FLASK_SECRET_KEY
 CORS(app)
 
-config = tf.ConfigProto(
-        device_count = {'GPU': 0}
-    )
+config = tf.ConfigProto(device_count = {'GPU': 0})
 sess = tf.Session(config=config)
 global graph
 graph = tf.get_default_graph()
@@ -23,12 +25,15 @@ set_session(sess)
 global model
 model = load_model('app/model/model.h5')
 
-labels = ['10', '100', '20', '200', '2000', '50', '500']
-UPLOAD_FOLDER = 'app/static/uploads/'
-app.secret_key = "12345678901"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+def upload_file_to_s3(file, filename):
+    s3 = boto3.resource('s3', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
+    try:
+        bucket = s3.Bucket(BUCKET)
+        bucket.Object(UPLOAD_FOLDER+filename).put(Body=file)
+        return True
+    except FileNotFoundError:
+        print("FileNotFoundError")
+        return False
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -50,32 +55,37 @@ def upload_image():
 
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        label = get_label('app/static/uploads/' + filename)
-        return render_template('index.html', filename=filename, prediction=label)
+        uploaded = upload_file_to_s3(file, filename)
+        if uploaded:
+            image_url = get_image_link(filename)
+            label = get_label(image_url)
+            return render_template('index.html', image_url=image_url, prediction=label)
+        else:
+            flash("Couldn't upload your image. Please try again!", 'alert-danger')
+            return redirect(request.url)
     else:
         flash('Allowed image types are -> png, jpg, jpeg', 'alert-danger')
         return redirect(request.url)
 
-@app.route('/display/<filename>')
-def display_image(filename):
-	return redirect(url_for('static', filename='uploads/' + filename), code=301)
-
-def get_label(filename):
+def get_label(image_url):
     global sess
     global graph
-    processed_image = process_image(filename)
+    processed_image = process_image(image_url)
     with graph.as_default():
         set_session(sess)
         classes = model.predict(processed_image)
-    label_classes = zip(labels, classes.tolist()[0])
+    label_classes = zip(LABELS, classes.tolist()[0])
     max_label_class = sorted(label_classes, key = lambda t: t[1])[-1]
     label = max_label_class[0]
     return label
 
-def process_image(filename):
-    img = load_img(filename, target_size=(150,150))
-    img = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
-    img = img.reshape(1,150,150,3).astype('float')
-    img /= 255
-    return img
+def process_image(image_url):
+    with urllib.request.urlopen(image_url) as url:
+        img = load_img(io.BytesIO(url.read()), target_size=(150, 150))
+        img = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
+        img = img.reshape(1,150,150,3).astype('float')
+        img /= 255
+        return img
+
+def get_image_link(filename):
+    return "https://indian-currency-prediction.s3.ap-south-1.amazonaws.com/uploads/" + filename
