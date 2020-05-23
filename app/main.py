26bd58ff6fd77199371c90be2046b1ab9f207bfe
Rohit Swami <rowhitswami1@gmail.com>
2020-05-23 11:12:30 +0530

    revert to initial commit

--- app/main.py
+++ app/main.py
@@ -2,38 +2,26 @@ from flask_cors import CORS
 import numpy as np
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array, load_img
-import urllib.request
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 import tensorflow as tf
 from keras.backend import set_session
-import boto3
-import io
 from config import *
-import urllib.request
 
 app = Flask(__name__)
 app.secret_key = FLASK_SECRET_KEY
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 CORS(app)
 
 config = tf.compat.v1.ConfigProto(device_count = {'GPU': 0})
 sess = tf.compat.v1.Session(config=config)
+
 global graph
 graph = tf.compat.v1.get_default_graph()
 set_session(sess)
 
 global model
-model = load_model('app/model/model.h5')
-
-def upload_file_to_s3(file, filename):
-    s3 = boto3.resource('s3', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
-    try:
-        bucket = s3.Bucket(BUCKET)
-        bucket.Object(UPLOAD_FOLDER+filename).put(Body=file)
-        return True
-    except FileNotFoundError:
-        print("FileNotFoundError")
-        return False
+model = load_model(MODEL)
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -55,41 +43,34 @@ def upload_image():
 
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        uploaded = upload_file_to_s3(file, filename)
-        if uploaded:
-            image_url = get_image_link(filename)
-            label = get_label(image_url)
-            return render_template('index.html', image_url=image_url, prediction=label)
-        else:
-            flash("Couldn't upload your image. Please try again!", 'alert-danger')
-            return redirect(request.url)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename) 
+        file.save(file_path)
+        label = get_label(file_path)
+        return render_template('index.html', filename=filename, prediction=label)
     else:
         flash('Allowed image types are -> png, jpg, jpeg', 'alert-danger')
         return redirect(request.url)
 
-def get_label(image_url):
+@app.route('/display/<filename>')
+def display_image(filename):
+	#print('display_image filename: ' + filename)
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+def get_label(filename):
     global sess
     global graph
-    processed_image = process_image(image_url)
-    with graph.as_default():
+    processed_image = process_image(filename)	
+    with graph.as_default():	
         set_session(sess)
         classes = model.predict(processed_image)
     label_classes = zip(LABELS, classes.tolist()[0])
     max_label_class = sorted(label_classes, key = lambda t: t[1])[-1]
     label = max_label_class[0]
     return label
-
-def process_image(image_url):
-    extension = image_url.split(".")[-1]
-    file_name = 'temp.' + extension
-    with urllib.request.urlopen(image_url) as url:
-        with open(os.path.join(BASE_DIR + TMP_FOLDER, file_name), 'wb+') as f:
-            f.write(url.read())
-    img = load_img(os.path.join(BASE_DIR + TMP_FOLDER, file_name), target_size=(150, 150))
+	
+def process_image(filename):
+    img = load_img(filename, target_size=(150,150))
     img = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
     img = img.reshape(1,150,150,3).astype('float')
     img /= 255
     return img
-
-def get_image_link(filename):
-    return "https://indian-currency-prediction.s3.ap-south-1.amazonaws.com/uploads/" + filename
--- config/__init__.py
+++ config/__init__.py
@@ -1,11 +1,14 @@
 import os
+
+# S3 Credentials
 AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
+BUCKET = 'indian-currency-prediction'
+REGION_HOST = 'ap-south-1'
+
+# Local Credentials
 LABELS = ['10', '100', '20', '200', '2000', '50', '500']
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-BUCKET = 'indian-currency-prediction'
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-UPLOAD_FOLDER = 'uploads/'
-TMP_FOLDER = '/static/tmp/'
-REGION_HOST = 'ap-south-1'
+MODEL = 'app/model/model.h5'
+UPLOAD_FOLDER = 'app/static/uploads/'
 FLASK_SECRET_KEY = 'Sssshhhhh.....!!!!'
