a7bd192490e4f8b892cb267d9379e2945daf6e51
Rohit Swami <rowhitswami1@gmail.com>
2020-05-23 22:16:18 +0530

    Add s3 support (#2)
    * add module
    
    * Add requirements
    
    * Add s3 support for images

---
+++ config/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+# S3 Credentials
+AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
+AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
+BUCKET = 'indian-currency-prediction'
+REGION_HOST = 'ap-south-1'
+UPLOAD_DIR = 'uploads/'
+
+# Local Credentials
+LABELS = ['10', '100', '20', '200', '2000', '50', '500']
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+UPLOAD_FOLDER = 'app/static/uploads/'
+FLASK_SECRET_KEY = 'Sssshhhhh.....!!!!'
+MODEL_PATH = 'app/model/model.h5'
---
+++ processing/__init__.py
@@ -0,0 +1,62 @@
+import boto3
+from config import *
+from PIL import Image
+import tensorflow as tf
+from keras.models import load_model
+from keras.backend import set_session
+from keras.preprocessing.image import img_to_array, load_img
+global graph
+global model
+
+# Tensorflow config
+config = tf.compat.v1.ConfigProto(device_count = {'GPU': 0})
+sess = tf.compat.v1.Session(config=config)
+graph = tf.compat.v1.get_default_graph()
+set_session(sess)
+model = load_model(MODEL_PATH)
+
+def get_label(file_path):
+    global sess
+    global graph
+    processed_image = process_image(file_path)
+    with graph.as_default():
+        set_session(sess)
+        classes = model.predict(processed_image)
+    label_classes = zip(LABELS, classes.tolist()[0])
+    max_label_class = sorted(label_classes, key = lambda t: t[1])[-1]
+    label = max_label_class[0]
+    return label
+
+def process_image(file_path):
+    img = load_img(file_path, target_size=(150,150))
+    img = img_to_array(img)
+    img = img.reshape(1,150,150,3).astype('float')
+    img /= 255
+    return img
+
+def upload_file_to_s3(file_path, filename):
+    s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
+    try:
+        compressed = compress_image(file_path)
+        if compressed:
+            with open(file_path, 'rb') as image:
+                s3.upload_fileobj(image, BUCKET, UPLOAD_DIR + filename)
+                return True
+        else:
+            return False
+    except FileNotFoundError:
+        return False
+
+def get_image_link(filename):
+    return "https://" + BUCKET + ".s3." + REGION_HOST + ".amazonaws.com/uploads/" + filename
+
+def compress_image(file_path):
+    try:
+        img = Image.open(file_path)
+        img_width = img.size[0] * 20 // 100
+        img_height = img.size[1] * 20 // 100
+        img = img.resize((img_width,img_height),Image.ANTIALIAS)
+        img.save(file_path, optimize=True,quality=50)
+        return True
+    except FileNotFoundError:
+        return False
