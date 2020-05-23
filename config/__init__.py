7adb9a0c7bf1e16af79185a8cf56bddd09e3122d
Rohit Swami <rowhitswami1@gmail.com>
2020-05-23 22:08:03 +0530

    Add requirements

--- config/__init__.py
+++ config/__init__.py
@@ -5,6 +5,7 @@ AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
 BUCKET = 'indian-currency-prediction'
 REGION_HOST = 'ap-south-1'
+UPLOAD_DIR = 'uploads/'
 
 # Local Credentials
 LABELS = ['10', '100', '20', '200', '2000', '50', '500']
