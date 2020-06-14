64d22e634e3e3596f1021691596633757130105d
Rohit Swami <rowhitswami1@gmail.com>
2020-06-14 13:49:27 +0530

    Change env variables

--- config/__init__.py
+++ config/__init__.py
@@ -3,15 +3,15 @@ import os
 # S3 Credentials
 AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
-BUCKET = 'indian-currency-prediction'
-REGION_HOST = 'ap-south-1'
-UPLOAD_DIR = 'uploads/'
-PRED_DIR = 'predictions/'
+BUCKET = os.environ.get("ICP_BUCKET")
+REGION_HOST = os.environ.get("ICP_BUCKET_REGION")
+UPLOAD_DIR = os.environ.get("ICP_UPLOAD_DIR")
+PRED_DIR = os.environ.get("ICP_PRED_DIR")
 
 # Local Credentials
 LABELS = ['10', '100', '20', '200', '2000', '50', '500']
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 UPLOAD_FOLDER = 'app/static/uploads/'
-FLASK_SECRET_KEY = os.urandom(12).hex()
+FLASK_SECRET_KEY = os.environ.get("ICP_FLASK_SECRET_KEY")
 MODEL_PATH = 'app/model/model.h5'
 WTF_CSRF_TIME_LIMIT = 1800
